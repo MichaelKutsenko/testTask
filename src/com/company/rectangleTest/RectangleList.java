6c8fc39c13920c10289f5c7b2c495de93ae8258d
Michael Kutsenko <m.kutsenko.work@gmail.com>
2016-12-23 12:38:43 +0200

    remade algorithm

--- src/com/company/rectangleTest/RectangleList.java
+++ src/com/company/rectangleTest/RectangleList.java
@@ -5,13 +5,13 @@ import java.util.*;
 public class RectangleList {
     private static final List<Rectangle> rectangles = new ArrayList<>();
 
-    public List<Rectangle> matchRectangleWithPoint(List<Point> points){
+    public List<Rectangle> matchRectangleWithPoint(List<Point> points) {
         List<Rectangle> matchedRectangles = new LinkedList<>();
 
         Collections.sort(rectangles, new Comparator<Rectangle>() {
             @Override
             public int compare(Rectangle o1, Rectangle o2) {
-                if (o1.getLeftBottomX() != o2.getLeftBottomX()){
+                if (o1.getLeftBottomX() != o2.getLeftBottomX()) {
                     return o1.getLeftBottomX() - o2.getLeftBottomX();
                 } else return o1.getLeftBottomY() - o2.getLeftBottomY();
             }
@@ -21,7 +21,7 @@ public class RectangleList {
         Collections.sort(points, new Comparator<Point>() {
             @Override
             public int compare(Point o1, Point o2) {
-                if (o1.getX() != o2.getX()){
+                if (o1.getX() != o2.getX()) {
                     return o1.getX() - o2.getX();
                 } else return o1.getY() - o2.getY();
             }
@@ -31,23 +31,23 @@ public class RectangleList {
         for (Point point : points) {
             for (int i = counter; i < rectangles.size(); i++) {
 
-                        int x = point.getX();
-                        int y = point.getY();
+                int x = point.getX();
+                int y = point.getY();
 
-                        int rectangleLeftX = rectangles.get(i).getLeftBottomX();
-                        int rectangleRightX = rectangles.get(i).getRightTopX();
-                        int rectangleBottomY = rectangles.get(i).getLeftBottomY();
-                        int rectangleTopY = rectangles.get(i).getRightTopY();
+                int rectangleLeftX = rectangles.get(i).getLeftBottomX(); //left side
+                int rectangleRightX = rectangles.get(i).getRightTopX(); //right side
+                int rectangleBottomY = rectangles.get(i).getLeftBottomY(); //bottom side
+                int rectangleTopY = rectangles.get(i).getRightTopY(); //top side
 
-                        if (x > rectangleRightX || y > rectangleTopY) {
-                            counter++;
-                            continue;
-                        } else if (x < rectangleLeftX || y < rectangleBottomY) {
-                            break;
-                        } else {
-                            matchedRectangles.add(rectangles.get(i));
-                            counter++;
-                            break;
+                if (x > rectangleRightX) {
+                    counter++;
+                    continue;
+                } else if (x < rectangleLeftX) {
+                    break;
+                } else if (y <= rectangleTopY && y >= rectangleBottomY) {
+                    matchedRectangles.add(rectangles.get(i));
+                    counter++;
+                    break;
                 }
             }
         }
